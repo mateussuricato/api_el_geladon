b6329b6980818125395041bf27efad7016020662
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2022-06-13 18:01:29 -0300

    swagger

--- src/routes/paleta.route.js
+++ src/routes/paleta.route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { readFile } from 'fs/promises';
 import {
   findAll,
   findById,
@@ -9,8 +10,13 @@ import {
 
 import { validId, validObjectBody } from '../middlewares/paleta.middleware.js';
 
+import  swaggerUi from 'swagger-ui-express';
+
+const json = JSON.parse(await readFile(new URL('../swagger.json', import.meta.url)));
+
 export const routes = express.Router();
 
+routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(json));
 routes.get('/paletas', findAll);
 routes.get('/paletas/:id', validId, findById);
 routes.post('/create', validObjectBody, createPaletaController);
